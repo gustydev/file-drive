d96299d35a2d345a7bb16512c0a8b5664d748c8a
gusty <gustavodiorlando@gmail.com>
2024-08-08 15:19:43 -0300

    Add form validation for all forms + set file size limit

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,11 +1,23 @@
-const multer  = require('multer')
-const upload = multer({ dest: './public/uploads/' })
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const prisma = require('../prisma/client');
+const multer  = require('multer')
+const maxSize = 3 * 1024 * 1024; // 3 MB
+const upload = multer({ dest: './public/uploads/', limits: {fileSize: maxSize} })
+
+exports.fileDetailGet = asyncHandler(async function(req, res, next) {
+    const file = await prisma.file.findUnique({where: {id: req.params.id}, include: {folder: true, owner: true}});
+    const folders = await prisma.folder.findMany();
+
+    res.render('fileDetail', {
+        file: file,
+        user: req.user,
+        folders: folders
+    })
+}) 
 
 exports.fileUpload = [
-    upload.single('file'),
+    upload.single('file'), // Add error handling for files larger than 3MB later
 
     asyncHandler(async function(req, res, next) {
         if (req.file) {
@@ -19,45 +31,66 @@ exports.fileUpload = [
                     folderId: req.body.folder || undefined
                 }
             })
-            console.log(file)
         }
+
         if (req.body.folder) {
-            res.redirect(`/folder/${req.body.folder}`);
+            res.redirect(`/folder/${req.body.folder}`)
         } else {
             res.redirect('/')
         }
     })
 ]
 
-exports.fileDetailGet = asyncHandler(async function(req, res, next) {
-    const file = await prisma.file.findUnique({where: {id: req.params.id}, include: {folder: true, owner: true}});
-    const folders = await prisma.folder.findMany();
+exports.fileDownload = [
+    body('fileId').custom(async (value) => {
+        const file = await prisma.file.findUnique({where: {id: value}});
 
-    res.render('fileDetail', {
-        file: file,
-        user: req.user,
-        folders: folders
-    })
-}) 
-
-exports.fileDownload = asyncHandler(async function(req, res, next) {
-    const file = await prisma.file.findUnique({where: {id: req.body.fileId}});
-    res.download(file.url, file.name, (err) => {
-        if (err) {
-            next(err)
+        if (!file) {
+            throw new Error(`Invalid file ID: ${value}`)
         }
-    })
-}) 
+    }),
+
+    asyncHandler(async function(req, res, next) {
+        const errors = validationResult(req);
+        
+        if (errors.isEmpty()) {
+            const file = await prisma.file.findUnique({where: {id: req.body.fileId}});
+            res.download(file.url, file.name, (err) => {
+                if (err) {
+                    next(err)
+                }
+            })
+        }
+        else {
+            const file = await prisma.file.findUnique({where: {id: req.params.id}, include: {folder: true, owner: true}});
+            const folders = await prisma.folder.findMany();
+        
+            res.render('fileDetail', {
+                file: file,
+                user: req.user,
+                folders: folders,
+                errors: errors.array()
+            })
+        }
+    }) 
+]
 
 exports.fileMove = [
+    body('fileId').custom(async (value) => {
+        const file = await prisma.file.findUnique({where: {id: value}});
+        if (!file) {
+            throw new Error(`Invalid file ID: ${value}`)
+        }
+    }),
+
     body('folder').custom(async (value) => {
         if (value === 'remove') {
             return;
         }
-        
+
         const folder = await prisma.folder.findUnique({where: {id: value}});
         if (!folder) {
-            throw new Error(`Invalid folder: ${value}`)
+            throw new Error(`Invalid folder ID: ${value}`)
         }
     }),
 
