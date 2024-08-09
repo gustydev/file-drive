66ebdc0c1aa89c38b81e2cf6cca4dc6106fdf9f0
gusty <gustavodiorlando@gmail.com>
2024-08-09 14:28:20 -0300

    Implement file upload to cloud

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -2,8 +2,13 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const prisma = require('../prisma/client');
 const multer  = require('multer')
+const storage = multer.memoryStorage();
 const maxSize = 3 * 1024 * 1024; // 3 MB
-const upload = multer({ dest: './public/uploads/', limits: {fileSize: maxSize} })
+const upload = multer({ storage: storage, limits: {fileSize: maxSize} })
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    secure: true
+});
 
 exports.fileDetailGet = asyncHandler(async function(req, res, next) {
     const file = await prisma.file.findUnique({where: {id: req.params.id}, include: {folder: true, owner: true}});
@@ -21,16 +26,29 @@ exports.fileUpload = [
 
     asyncHandler(async function(req, res, next) {
         if (req.file) {
-            await prisma.file.create({
-                data: {
-                    ownerId: req.user.id,
-                    name: req.file.originalname,
-                    type: req.file.mimetype,
-                    size: req.file.size,
-                    url: req.file.path, // Later this should be a cloudinary url probably
-                    folderId: req.body.folder || undefined
-                }
-            })
+            console.log(req.file)
+            const data = {
+                ownerId: req.user.id,
+                name: req.file.originalname,
+                type: req.file.mimetype,
+                size: req.file.size,
+                url: undefined,
+                folderId: req.body.folder || undefined
+            };
+
+            await new Promise((resolve) => {
+                cloudinary.uploader.upload_stream({resource_type: 'auto'}, (error, uploadResult) => {
+                    return resolve(uploadResult);
+                }).end(req.file.buffer);
+            }).then((uploadResult) => {
+                console.log(`Buffer upload_stream wth promise success - ${uploadResult.public_id}`);
+                data.url = uploadResult.secure_url;
+            });
+
+            const file = await prisma.file.create({
+                data: data
+            });
+            console.log(file)
         }
 
         if (req.body.folder) {
